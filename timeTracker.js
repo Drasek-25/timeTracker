e95f9c87a80bd5a52df8a54f7a5b32eb76a4b9b5
Patrick Marschlowitz <p.marschlowitz@gmail.com>
2020-02-03 20:42:04 -0800

    resolution of Issue#2, #4, #5

--- timeTracker.js
+++ timeTracker.js
@@ -2,44 +2,70 @@ let timerList = [
     [],
     [],
 ];
-
-
+let activeTimers = [];
 let rowCounter = 2;
+
+
 
 function addRow() {
     let newRow = document.getElementById("row0");
     let cln = newRow.cloneNode(true);
     cln.id = `row${rowCounter}`;
+    cln.style.border = "#EDA920 1px solid"
+    cln.style.backgroundColor = "transparent";
     timerList.push([])
     rowCounter++;
     document.getElementById("wrapper").appendChild(cln);
     console.log(`Created row${rowCounter}`);
 }
 
+
+
+function rowDigits(row) {
+    if (row.length > 3) {
+        rowNumber = row.charAt(4);
+        rowNumber = `${row.charAt(3)}${(rowNumber)}`;
+        rowNumber = parseInt(rowNumber);
+        return rowNumber;
+    } else {
+        rowNumber = row.charAt(3);
+        rowNumber = parseInt(rowNumber);
+        return rowNumber;
+    }
+}
+
+
+//potential bug here
+//the problem would be related to the timers getting fucked up by changes to this code
+//changes included addition of rowDigits() and the first if statment
 function deleteRow(elem) {
     let row = elem.parentNode.parentNode.id;
-
-    if (row == 'row0') {
-        alert('cannot remove row0');
-        return;
-
-    } else {
-        console.log(`removed ${row}`);
+    let rowNumber = rowDigits(row);
+    if (activeTimers.indexOf(row) > -1) {
+        startStop(elem);
+        console.log(`Removed row${rowNumber}`);
         let removeRow = document.getElementById(row);
         removeRow.remove();
-        rowNumber = row.charAt(row.length - 1);
+        timerList.splice(rowNumber, 1)
+    } else if (row == 'row0') {
+        alert('cannot remove row0');
+        return;
+    } else {
+        rowNumber = rowDigits(row);
+        console.log(`Removed row${rowNumber}`);
+        let removeRow = document.getElementById(row);
+        removeRow.remove();
         timerList.splice(rowNumber, 1)
     }
 }
 
-let activeTimers = [];
-let end;
-let start;
+
 
 function startStop(elem) {
-
+    let end;
+    let start;
     let row = elem.parentNode.parentNode.id;
-    let rowNumber = row.charAt(row.length - 1);
+    let rowNumber = rowDigits(row);
     if (activeTimers.indexOf(row) == -1) {
         document.getElementById(row).style.backgroundColor = "#EDA920";
         start = window.performance.now();
@@ -95,7 +121,15 @@ function checkBox(elem) {
 
 }
 
+
+
 function savePage() {
+
+    if (activeTimers.length > 0) {
+        alert('End all timers before saving.');
+        return;
+    }
+
     let csvRowChange = '\r\n'
     let saveData = ['',
         document.getElementById("header1").innerText,
@@ -118,12 +152,12 @@ function savePage() {
         saveData.push(time);
         saveData.push(csvRowChange);
     }
+
     let d = new Date();
-    let month = d.getMonth()+1;
+    let month = d.getMonth() + 1;
     let day = d.getDate();
     let year = d.getFullYear();
     let saveDate = `${month}.${day}.${year}`;
-    console.log(d)
     var file = new Blob([saveData], {
         encoding: "UTF-8",
         type: "text/csv;charset=UTF-8"
@@ -142,4 +176,33 @@ function savePage() {
             window.URL.revokeObjectURL(url);
         }, 0);
     }
+}
+
+
+
+function editTitle() {
+    let header1 = document.getElementById('header1');
+    let header2 = document.getElementById('header2');
+    let header3 = document.getElementById('header3');
+    let header4 = document.getElementById('header4');
+    let newHeader1 = prompt(`Please provide a new name for ${header1.innerText}.`)
+    if (newHeader1 === null) {
+        return;
+    }
+    let newHeader2 = prompt(`Please provide a new name for ${header2.innerText}.`)
+    if (newHeader2 === null) {
+        return;
+    }
+    let newHeader3 = prompt(`Please provide a new name for ${header3.innerText}.`)
+    if (newHeader3 === null) {
+        return;
+    }
+    let newHeader4 = prompt(`Please provide a new name for ${header4.innerText}.`)
+    if (newHeader4 === null) {
+        return;
+    }
+    header1.innerText = newHeader1
+    header2.innerText = newHeader2
+    header3.innerText = newHeader3
+    header4.innerText = newHeader4
 }
